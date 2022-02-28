328a70637960d7f38557709f0fc9ab5676ff309b
Taeho Choi <60216512+taeho-choi@users.noreply.github.com>
2022-02-28 20:53:09 +0900

    JavaScript 애니메이션 중단과 재개, README.md 작성

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const circle = document.querySelector("#circle");
+const article = document.querySelectorAll("article");
+
+for(let el of article) {
+    el.addEventListener("mouseenter",e=> {
+        circle.style.animationPlayState = "paused";
+    });
+
+    el.addEventListener("mouseleave",e=> {
+        circle.style.animationPlayState = "running";
+    })
+}   
